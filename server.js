2a821425f7b5e70b3ca3d85e9d4215f7e0c6f238
vi.nguyendt@cloudgo.vn <nguyenduongthevi31415@gmail.com>
2025-04-07 18:37:55 +0700

    Update

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const server = createServer(app);
 app.use(express.json());
 app.use(cors());
 app.use("/api/items", itemRoutes);
-
+app.get("/", (req, res) => {
+  res.send("API đang chạy!");
+});
 const PORT = process.env.PORT || 8800;
 server.listen(PORT, () => {
   console.log(`Server đang chạy trên PORT: ${PORT}`);
